0d916b97bdbd8099c9ca90c5e796ae9a8a1d9c74
SKR301 <chunnu.bihat@gmail.com>
2022-03-04 21:51:15 +0530

    updated pandit adapter

--- app/src/main/java/com/aksahyaap/panditbook/users/PanditAdapter.java
+++ app/src/main/java/com/aksahyaap/panditbook/users/PanditAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,15 +14,15 @@ import com.aksahyaap.panditbook.R;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PanditsAdapter extends RecyclerView.Adapter<PanditsAdapter.ViewHolder> {
+public class PanditAdapter extends RecyclerView.Adapter<PanditAdapter.ViewHolder> {
 
     List<Pandit> panditList = new ArrayList<>();
     Context context;
 
-    public PanditsAdapter(Context context){
+    public PanditAdapter(Context context){
         this.context = context;
     }
-    public void setPanditsList(List<Pandit>panditList){
+    public void setPanditList(List<Pandit>panditList){
         this.panditList = panditList;
     }
 
@@ -33,6 +34,8 @@ public class PanditsAdapter extends RecyclerView.Adapter<PanditsAdapter.ViewHold
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Pandit pandit = panditList.get(position);
+        holder.panditDet(pandit);
     }
 
     @Override
@@ -41,8 +44,20 @@ public class PanditsAdapter extends RecyclerView.Adapter<PanditsAdapter.ViewHold
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView txtView_panditName;
+        TextView txtView_panditRating;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            txtView_panditName = itemView.findViewById(R.id.txtView_panditName);
+            txtView_panditRating = itemView.findViewById(R.id.txtView_panditRating);
+        }
+
+        public void panditDet(Pandit pandit){
+            String panditName = pandit.getPanditName();
+            float panditRating = pandit.getPanditRating();
+            txtView_panditName.setText(panditName);
+            txtView_panditName.setText(String.valueOf(panditRating));
         }
     }
 }
